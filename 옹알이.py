3c2c0606596af4a842b742df9cd82af345d9d855
umoo <nnmnn1022@gmail.com>
2022-10-06 10:05:26 +0900

    1006_study

--- 옹알이.py
+++ 옹알이.py
@@ -1,22 +1,43 @@
-# def solution(n, left, right):
-#     arr = []
-#     start = left // n + 1
-#     end = right // n + 1
+#  "aya", "ye", "woo", "ma" 네 가지 발음과 네 가지 발음을 조합해서 만들 수 있는 발음
+# 연속해서 발음할 수 없음
 
-#     for i in range(start, end+1):
-#         arr.extend([i]*i)
-#         arr.extend(list(range(i+1, n+1)))
+def solution(babbling):
+    cando = ['aya', 'ye', 'woo', 'ma']
 
-#     return arr[left%n:(end-start)*n+right%n+1]
+    # 옹알이하고 남은 발음 = []
+    remain = ''
+    # 말할 수 있는 단어 개수
+    n = 0
 
-def solution(n, left, right):
-    answer = []
-    for i in range(left,right+1):
-        answer.append(max(i//n,i%n)+1)
-    return answer
+    # for
+    for b in babbling:
+        remain = b
+        i = 0
+        # 현재 발음 = ''
+        now = ''
+        # while 돌리기
+        while(True):
+            # babbling이 cando 중 하나로 시작하면
+            if not remain:
+                n += 1
+                break
+            elif i >= 4 :
+                break
+            elif now != cando[i] and remain.startswith(cando[i]):
+                # 현재 발음 넣기
+                now = cando[i]
+                len_now = len(now)
+                # 발음한 부분 빼기
+                remain = remain[len_now:]
+                i = -1
 
-print(solution(3, 2, 5))
-print(solution(4, 7, 14))
+            i += 1
+
+    return n
+
+
+print(solution(["aya", "yee", "u", "maa"]))
+print(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))
 # print(solution(10, 7, 14))
 
 # 1 2 3 4 5 6
